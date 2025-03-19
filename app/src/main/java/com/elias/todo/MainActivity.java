09189a3d758917eb4d39fa6d3c66ac23c89c13cb
Elias Orlow <orlow.elias03@gmail.com>
2025-03-19 14:10:43 +0100

    feat(dialog): add bottom sheet for creating tasks and folders
    - Initialize bottom sheet layout
    - Add new vector icons for dialog UI
    - Apply animation and transparency settings for smooth UI experience

--- app/src/main/java/com/elias/todo/MainActivity.java
+++ app/src/main/java/com/elias/todo/MainActivity.java
@@ -1,6 +1,12 @@
 package com.elias.todo;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -25,5 +31,51 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_history, R.id.navigation_calendar).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCreateDialog();
+            }
+        });
+    }
+
+    private void showCreateDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottomsheetlayout);
+
+        LinearLayout layoutNewTask = dialog.findViewById(R.id.layoutNewTask);
+        LinearLayout layoutNewFolder = dialog.findViewById(R.id.layoutNewFolder);
+        ImageView cancelButton = dialog.findViewById(R.id.cancelButton);
+
+        layoutNewTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this, "New Task create", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        layoutNewFolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this, "New Folder create", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(android.view.Gravity.BOTTOM);
     }
 }
