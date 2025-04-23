6ccde9a209fb0bb97f681863fba085c736fac71f
Elias Orlow <orlow.elias03@gmail.com>
2025-04-23 11:36:34 +0200

    feat(ui): add new task creation sheet
    - Initialize create_task_sheet layout
    - Add vector icons for dialog UI
    - Implement date picker dialog

--- app/src/main/java/com/elias/todo/MainActivity.java
+++ app/src/main/java/com/elias/todo/MainActivity.java
@@ -1,9 +1,13 @@
 package com.elias.todo;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -16,9 +20,13 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.elias.todo.databinding.ActivityMainBinding;
 
+import java.util.Calendar;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Calendar calendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dialog.dismiss();
-                Toast.makeText(MainActivity.this, "New Task create", Toast.LENGTH_LONG).show();
+                showNewTaskDialog();
             }
         });
 
@@ -77,5 +85,78 @@ public class MainActivity extends AppCompatActivity {
         dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
         dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
         dialog.getWindow().setGravity(android.view.Gravity.BOTTOM);
+    }
+
+    private void showNewTaskDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.create_task_sheetlayout);
+
+        ImageView cancelButton = dialog.findViewById(R.id.cancelButton);
+        EditText edit_description = dialog.findViewById(R.id.edit_description);
+        ImageView star_favorite = dialog.findViewById(R.id.star_favorite);
+        EditText edit_time = dialog.findViewById(R.id.edit_time);
+        Button btn_pick_datetime = dialog.findViewById(R.id.btn_pick_datetime);
+        EditText edit_folder = dialog.findViewById(R.id.edit_folder);
+        Button create_new_task = dialog.findViewById(R.id.create_new_task);
+
+        AtomicBoolean isFavorite = new AtomicBoolean(false);
+
+        cancelButton.setOnClickListener(view -> dialog.dismiss());
+
+        star_favorite.setOnClickListener(view ->  {
+                boolean newValue = !isFavorite.get();
+                isFavorite.set(newValue);
+                if (newValue) {
+                    star_favorite.setImageResource(R.drawable.ic_star_filled_24);
+                } else {
+                    star_favorite.setImageResource(R.drawable.ic_star_border_24);
+                }
+            }
+        );
+
+        edit_time.setOnClickListener(view -> showDataTimePicker(edit_time));
+        btn_pick_datetime.setOnClickListener(view -> showDataTimePicker(edit_time));
+
+        create_new_task.setOnClickListener(view -> {
+                Toast.makeText(MainActivity.this, "New Task created", Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+            }
+        );
+
+        dialog.show();
+        dialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(android.view.Gravity.BOTTOM);
+    }
+
+    private void showDataTimePicker(EditText editTime) {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
+                R.style.CustomDatePickerDialog,
+                (view, year, month, dayOfMonth) -> {
+                    calendar.set(Calendar.YEAR, year);
+                    calendar.set(Calendar.MONTH, month);
+                    calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                    TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this,
+                            R.style.CustomDatePickerDialog,
+                            (view1, hourOfDay, minute) -> {
+                                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                                calendar.set(Calendar.MINUTE, minute);
+
+                                String formattedDateTime = String.format("%02d-%02d-%04d %02d:%02d", dayOfMonth, month + 1, year, hourOfDay, minute);
+                                editTime.setText(formattedDateTime);
+                            },
+                            calendar.get(Calendar.HOUR_OF_DAY),
+                            calendar.get(Calendar.MINUTE),
+                            true);
+                    timePickerDialog.show();
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.show();
     }
 }
